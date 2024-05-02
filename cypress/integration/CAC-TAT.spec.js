5020c06190cef059e9601d1a472a225670c3a5b4
nathancrs <166561046+nathancrs@users.noreply.github.com>
2024-05-02 02:08:09 -0300

    Novo teste ''

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -12,6 +12,8 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     })
 
     it('preenche os campos obrigatórios e envia o formulário', function () {
+        cy.clock()
+
         const longText = 'aoisjfaoifjoajfoasjfoasjfoasijfaosijfaoifjoasijfoasjfoasijfoasijfoasjfoaifjoasijfoasfjaoisfj'
         cy.get('#firstName').type('Nathan')
         cy.get('#lastName').type('Santos')
@@ -20,6 +22,10 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.contains('button', 'Enviar').click()
 
         cy.get('.success').should('be.visible')
+
+        cy.tick(3000)
+
+        cy.get('.success').should('not.be.visible')
     })
 
     it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
@@ -169,14 +175,75 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#privacy a').should('have.attr', 'target', '_blank')
     })
 
-    it('acessa a página da política de privacidade removendo o target e então clicando no link', function () {
-        cy.get('#privacy a')
-            .invoke('removeAttr', 'target')
-            .click()
+    Cypress._.times(10, function () {
+        it('acessa a página da política de privacidade removendo o target e então clicando no link', function () {
+            cy.get('#privacy a')
+                .invoke('removeAttr', 'target')
+                .click()
 
-        cy.contains('Talking About Testing').should('be.visible')
+            cy.contains('Talking About Testing').should('be.visible')
+        })
+
     })
 
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+        cy.get('.success')
+            .should('not.be.visible')
+            .invoke('show')
+            .should('be.visible')
+            .and('contain', 'Mensagem enviada com sucesso.')
+            .invoke('hide')
+            .should('not.be.visible')
+        cy.get('.error')
+            .should('not.be.visible')
+            .invoke('show')
+            .should('be.visible')
+            .and('contain', 'Valide os campos obrigatórios!')
+            .invoke('hide')
+            .should('not.be.visible')
+    })
+
+    it('preenche a area de texto usando o comando invoke', function () {
+        const longText = Cypress._.repeat('123456789', 20)
+
+        cy.get('#open-text-area')
+            .invoke('val', longText)
+            .should('have.value', longText)
+
+    })
+
+    it('faz uma requisição HTTP', function () {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should(function (response) {
+                const { status, statusText, body } = response
+                expect(status).to.equal(200)
+                expect(statusText).to.equal('OK')
+                expect(body).to.include('CAC TAT')
+            })
+    })
+
+    it('Encontre o gato', function () {
+        cy.get('#cat')
+            .invoke('show')
+            .should('be.visible')
+        cy.get('#title')
+            .invoke('text', 'CAT TAT')
+        cy.get('#subtitle')
+            .invoke('text', 'Eu amo gatos <3')
+    })
+
+    //it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function () {
+    //cy.get('#firstName')
+    //.type('Nathan')
+    //.invoke('hide')
+    //.should('not.be.visible')
+    //cy.contains('button', 'Enviar').click()
+    //.should('be.visible', 'Valide os campos obrigatórios!')
+    //cy.get('#firstName')
+    //.invoke('show')
+    //.type('Nathan')
+    //.should('be.visible')
+    // })
     //it('testa a página da política de privacidade de forma independente', function () {
     //const longText = 'Não salvamos dados submetidos no formulário da aplicação CAC TAT. Utilzamos as tecnologias HTML, CSS e JavaScript, para simular uma aplicação real. No entanto, a aplicação é um exemplo, sem qualquer persistência de dados, e usada para fins de ensino.'
     //cy.get('#privacy a')
